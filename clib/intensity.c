a2331efe680371dc6b389f86e1d3bdd4569bf4bd
Mateusz Zdunek <zdunek.mateusz92@gmail.com>
2019-06-21 16:43:45 +0200

    all intensity sensors works!

--- clib/intensity.c
+++ clib/intensity.c
@@ -6,7 +6,7 @@
 #include <pigpio.h>
 #include "intensity.h"
 
-int fd_0, fd_1, fd_2, fd_3;
+int fd_1, fd_2, fd_3, fd_4;
 
 void intSetup() {
 	gpioInitialise();
@@ -14,37 +14,37 @@ void intSetup() {
 	//fd_1 = wiringPiI2CSetup(DEVICE_1);
 	//wiringPiI2CWrite(fd_0, CONTINUOUS_LOW_RES_MODE);
 	//wiringPiI2CWrite(fd_1, CONTINUOUS_LOW_RES_MODE);
-	fd_0 = i2cOpen(1, DEVICE_0, 0);
-	fd_1 = i2cOpen(1, DEVICE_1, 0);
-	fd_2 = i2cOpen(0, DEVICE_0, 0);
-	fd_3 = i2cOpen(0, DEVICE_1, 0);
-	//i2cWriteByte(fd_0, CONTINUOUS_LOW_RES_MODE);
-	//i2cWriteByte(fd_1, CONTINUOUS_LOW_RES_MODE);
-	//i2cWriteByte(fd_2, CONTINUOUS_LOW_RES_MODE);
-	//i2cWriteByte(fd_3, CONTINUOUS_LOW_RES_MODE);
+	fd_1 = i2cOpen(1, DEVICE_0, 0);
+	fd_2 = i2cOpen(1, DEVICE_1, 0);
+	fd_3 = i2cOpen(0, DEVICE_0, 0);
+	fd_4 = i2cOpen(0, DEVICE_1, 0);
+	i2cWriteByte(fd_1, CONTINUOUS_LOW_RES_MODE);
+	i2cWriteByte(fd_2, CONTINUOUS_LOW_RES_MODE);
+	i2cWriteByte(fd_3, CONTINUOUS_LOW_RES_MODE);
+	i2cWriteByte(fd_4, CONTINUOUS_LOW_RES_MODE);
 }
 
 void intClose() {
-	i2cClose(fd_0);
 	i2cClose(fd_1);
 	i2cClose(fd_2);
 	i2cClose(fd_3);
+	i2cClose(fd_4);
 }
 
 float intGetIntensity(int sensNum) {
 	switch (sensNum) {
 		case 1:
 		// Left sensor:
-			return intCountIntensity(fd_0, DEVICE_0);
+			return intCountIntensity(fd_1, DEVICE_0);
 		case 2:
 		// Center-left sensor:
-			return intCountIntensity(fd_1, DEVICE_1);
+			return intCountIntensity(fd_2, DEVICE_1);
 		case 3:
 		// Center sensor:
-			return intCountIntensity(fd_2, DEVICE_0);
+			return intCountIntensity(fd_3, DEVICE_0);
 		case 4:
 		// Center-right sensor:
-			return intCountIntensity(fd_3, DEVICE_1);
+			return intCountIntensity(fd_4, DEVICE_1);
 		default:
 			return printf("Wrong number of intensity sensor!");
 	}
@@ -72,7 +72,6 @@ float intCountIntensity(int fd, int device) {
 	int result;
 	float intensity;
 
-	//i2cWriteByte(fd, CONTINUOUS_LOW_RES_MODE);
 	usleep(INT_DELAY);
 	result = i2cReadWordData(fd, device);
 	
@@ -82,8 +81,6 @@ float intCountIntensity(int fd, int device) {
 	{
 		printf("Error.  Errno is: %d \n", errno);
 	}
-	
-	//intClose();
 	
 	return intensity;
 }
--- clib/main.c
+++ clib/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "ultrasonic.c"
 #include "intensity.c"
-//#include "acceleration.c"
+#include "acceleration.c"
 #include "servo.c"
 
 float randfloat(float a)
@@ -19,13 +19,11 @@ int main() {
     
     while (i == 0) {
         
-        printf("US: %f %f %f %f %f INT: %f %f %f \n",
+        printf("US: %f %f %f %f %f INT: %f %f %f %f \n",
         usGetDistance(1), usGetDistance(2), usGetDistance(3),
         usGetDistance(4), usGetDistance(5), intGetIntensity(1), 
-        intGetIntensity(2), intGetIntensity(4));
+        intGetIntensity(2), intGetIntensity(3), intGetIntensity(4));
         //accGetAccX(), accGetAccY());
-        
-        // | Intense: %f %f     | Acceleration: %f %f \n"
         
         svRide(randfloat(1));
         svRide(randfloat(-1));
